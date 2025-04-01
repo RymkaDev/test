2f0a20df153cfe29e76e30b40e2cfb66a27b8057
RymkaDev <sebastien@sablon.pro>
2025-04-01 21:34:53 +0200

    Bypass

--- L2Ext/KeyEngine.h
+++ L2Ext/KeyEngine.h
@@ -1,50 +1,55 @@
+
 #pragma once
+
+#include <string>
 
 namespace KeyEngine
 {
-	class CHardwareId
-	{
-		static const int SIZE = 8;
-		char m_Part[SIZE];
-	public:
-		CHardwareId();
-		CHardwareId(std::string hw);
-		void generate();
-		std::string toString();
-		const char& operator[](size_t pos) const;
-	};
-
-	int32_t getCharIndex(char c);
-	char getCharFromIndex(int32_t index);
-
-	enum State
-	{
-		StateNone = 0,
-		StateValid = 1,
-		StateInvalid = 2
-	};
-}
-
-class CKeyEngine
-{
-        CKeyEngine();
-        CKeyEngine(const CKeyEngine& copy);
-		CKeyEngine& operator = (const CKeyEngine& other);
-		
-		bool m_Loaded;
-		std::string m_Name;
-		std::string m_Key;
-		KeyEngine::CHardwareId m_Hardware;
-		KeyEngine::State m_State;
-		mutable bool m_Validated;		
-
-		void loadINI();
-		void writeINI();
-		bool isValid(std::string name, std::string key, const KeyEngine::CHardwareId& hardware) const;
+    class CHardwareId
+    {
     public:
+        enum { SIZE = 8 };
+
+        CHardwareId();
+        CHardwareId(std::string hw);
+
+        std::string toString();
+        const char& operator[](size_t pos) const;
+        void generate();
+
+    private:
+        char m_Part[SIZE];
+    };
+
+    class CKeyEngine
+    {
+    public:
+        enum State
+        {
+            StateNone,
+            StateInvalid,
+            StateValid
+        };
+
         static CKeyEngine& getInstance();
-		void init();
-		bool validated() const { return m_Validated; }
-		KeyEngine::State getState() const { return m_State; }
-		void showInvalidKeyMessage();
-};
+
+        void init();
+        bool isValid(std::string name, std::string key, const CHardwareId& hw) const;
+
+    private:
+        CKeyEngine();
+
+        void loadINI();
+        void writeINI();
+        void showInvalidKeyMessage();
+
+        mutable bool m_Validated;
+        std::string m_Name;
+        std::string m_Key;
+        CHardwareId m_Hardware;
+        State m_State;
+    };
+
+    int32_t getCharIndex(char c);
+    char getCharFromIndex(int32_t index);
+} // namespace KeyEngine
